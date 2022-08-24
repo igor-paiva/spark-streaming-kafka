f6413d3b2d762a8df2adb2232164b8fd0bd89b84
igor-paiva <igorbatistapaiva@outlook.com>
2022-08-24 14:51:59 -0300

    Adds create topic command in start script

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 docker network create spark-cluster || true
 
 docker-compose up -d
+
+# to create "sentences" topic
+docker exec -it spark_kafka_socket_1 bash kafka_2.13-3.2.1/bin/kafka-topics.sh --create --topic sentences --bootstrap-server kafka-server:9092
